8fb7f2aebff780426f97016fb6c1a47d4ec82652
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-25 13:27:27 +0300

    enables users to update their profile once they have logged into their accounts

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,3 +8,7 @@ class BlogPostsForm(FlaskForm):
     blog_post = TextAreaField('Blog Post', validators=[Required()])
     author=StringField("Author", validators=[Required()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
