6cf243088970483e14941856d1162144b54d911d
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-27 09:56:53 +0300

    added our secret key to our configuration

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     debug = True
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/blog'
 
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+export SECRET_KEY="gradwftformsecret"
 export MAIL_USERNAME="flaskjoe5@gmail.com"
 export MAIL_PASSWORD="Statistics163"
 python3 manage.py server
